c36001492a67e0eb42ac61f6e7ca1311a06e19af
Aaron Kravitz <arkravitz@gmail.com>
2013-05-20 19:13:04 -0700

    added comments and modified readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 tweetbot
 ========
-This program tweets the top link from reddit.com/r/funny and posts the
-picture's link to Twitter. Usage is python auto_tweet.py.
+This program tweets the top imgur link from reddit.com/r/funny to
+the users twitter. Usage is python auto_tweet.py.
+
+To set up, you'll need to install tweepy, and set up OAuth with twitter here: https://dev.twitter.com/apps.
+For more information on OAuth, check here: https://dev.twitter.com/docs/auth/oauth/faq.
--- auto_tweet.py
+++ auto_tweet.py
@@ -2,23 +2,26 @@ import tweepy
 import json
 import urllib2
 import random
-from apscheduler.scheduler import Scheduler # You'll need apscheduler for this
-from datetime import datetime
 
 def post_twitter(status):
+	''' Logs into twitter using the tweepy api and posts the parameter status'''
+
+	# Find these after creating an app on twitter
 	consumer_key=""
 	consumer_secret=""
-
 	access_token=""
 	access_token_secret=""
 
+	# Authorize for twitter
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token, access_token_secret)
 	api = tweepy.API(auth)
 
+	# Post the status
 	api.update_status(status)
 
 def get_top_r_funny():
+	''' Gets the top r/funny imgur post from reddit and returns the url of the post. '''
 	data = json.load(urllib2.urlopen("http://www.reddit.com/r/funny.json"))
 	posts = [post['data'] for post in data['data']['children']]
 	for post in posts:
@@ -26,14 +29,18 @@ def get_top_r_funny():
 			return str(post['url'])
 
 def get_funny_phrase():
+	''' Returns a random phrase from the list of phrases. '''
 	phrases = ['lol', 'smh', 'check it', 'haha', 'I can\'t deal with this!', 'Too funny', 'Woah', 'too much']
 	return random.choice(phrases)
 
 def get_random_hashtag():
+	''' Returns a random hashtag from the list of hashtags. '''
 	hashtags = ["#momquotes", '#AwkwardDate', '#LMAO', '#TheInternet']
 	return random.choice(hashtags)
 
 def post_tweet():
+	''' Adds together the phrase, the top reddit link, and the hashtag
+	and then posts the result to twitter. '''
 	phrase = get_funny_phrase()
 	top_link = get_top_r_funny()
 	hashtag = get_random_hashtag()
